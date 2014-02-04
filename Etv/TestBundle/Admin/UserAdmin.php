501c8801595b39dba8c71bccb970cc2d23acb960
bpec <bpecsi77@gmail.com>
2014-02-04 14:08:57 +0000

    ..

---
+++ Etv/TestBundle/Admin/UserAdmin.php
@@ -0,0 +1,42 @@
+<?php
+// vendor/etv/test/Etv/TestBundle/Admin/UserAdmin.php
+
+namespace Etv\TestBundle\Admin;
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+
+class UserAdmin extends Admin
+{
+    // Fields to be shown on create/edit forms
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('username', 'text', array('label' => 'User name'))
+            //->add('firstname', 'entity', array('class' => 'Etv\TestBundle\Admin\Entity\User'))
+           ->add('firstname', 'text', array('label' => 'First name'))
+           ->add('lastname', 'text', array('label' => 'Last name'))
+        ;
+    }
+
+    // Fields to be shown on filter forms
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('username')
+            ->add('firstname')
+            ->add('lastname')
+        ;
+    }
+
+    // Fields to be shown on lists
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->addIdentifier('username')
+            ->add('firstname')
+            ->add('lastname')
+        ;
+    }
+}
---
+++ Etv/TestBundle/Entity/User.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace Etv\TestBundle\Entity;
+
+/**
+ * User
+ */
+class User
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $username;
+
+    /**
+     * @var string
+     */
+    private $firtname;
+
+    /**
+     * @var string
+     */
+    private $lastname;
+
+    /**
+     * @var string
+     */
+    private $roles;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set username
+     *
+     * @param string $username
+     *
+     * @return User
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * Get username
+     *
+     * @return string 
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * Set firtname
+     *
+     * @param string $firtname
+     *
+     * @return User
+     */
+    public function setFirtname($firtname)
+    {
+        $this->firtname = $firtname;
+
+        return $this;
+    }
+
+    /**
+     * Get firtname
+     *
+     * @return string 
+     */
+    public function getFirtname()
+    {
+        return $this->firtname;
+    }
+
+    /**
+     * Set lastname
+     *
+     * @param string $lastname
+     *
+     * @return User
+     */
+    public function setLastname($lastname)
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    /**
+     * Get lastname
+     *
+     * @return string 
+     */
+    public function getLastname()
+    {
+        return $this->lastname;
+    }
+
+    /**
+     * Set roles
+     *
+     * @param string $roles
+     *
+     * @return User
+     */
+    public function setRoles($roles)
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * Get roles
+     *
+     * @return string 
+     */
+    public function getRoles()
+    {
+        return $this->roles;
+    }
+}
+
